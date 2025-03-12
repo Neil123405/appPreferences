f0666c64ef48ddbef6de37e0c87f885540455457
jspe2ba13 <jspe2ba13@scs.local>
2025-03-12 11:31:40 +0800

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  option1 = false;
+  option2 = false;
+  option3 = false;
+  rangeValue = 50;
+  checkbox1 = false;
+  checkbox2 = false;
+  checkbox3 = false;
+  radioOption = 'option1';
 
   constructor() {}
 
+  async ngOnInit() {
+    await this.loadSettings();
+  }
+
+  async saveSettings() {
+    await Preferences.set({
+      key: 'settings',
+      value: JSON.stringify({
+        option1: this.option1,
+        option2: this.option2,
+        option3: this.option3,
+        rangeValue: this.rangeValue,
+        checkbox1: this.checkbox1,
+        checkbox2: this.checkbox2,
+        checkbox3: this.checkbox3,
+        radioOption: this.radioOption,
+      }),
+    });
+  }
+
+  async loadSettings() {
+    const { value } = await Preferences.get({ key: 'settings' });
+    if (value) {
+      const settings = JSON.parse(value);
+      this.option1 = settings.option1;
+      this.option2 = settings.option2;
+      this.option3 = settings.option3;
+      this.rangeValue = settings.rangeValue;
+      this.checkbox1 = settings.checkbox1;
+      this.checkbox2 = settings.checkbox2;
+      this.checkbox3 = settings.checkbox3;
+      this.radioOption = settings.radioOption;
+    }
+  }
+
+  async resetSettings() {
+    this.option1 = false;
+    this.option2 = false;
+    this.option3 = false;
+    this.rangeValue = 50;
+    this.checkbox1 = false;
+    this.checkbox2 = false;
+    this.checkbox3 = false;
+    this.radioOption = 'option1';
+    await this.saveSettings();
+  }
 }
